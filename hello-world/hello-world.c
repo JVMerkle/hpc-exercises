85e3473e31dada278d20c8b871342d3391bfef03
Julian Merkle <juli.merkle@gmail.com>
2018-02-17 13:26:56 +0100

    A01d

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -4,9 +4,13 @@
 #include <omp.h>
 
 int main(int argc, char **argv) {
-	
-	#pragma omp parallel num_threads(4)
-  printf("Hello World from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+	char hello_world[5][50] = {"Hallo Welt!", "Hola mundo", "Hello World", "Hej varlden", "Bonjour tout le monde" };
+
+	#pragma omp parallel for num_threads(4)
+	for( int i = 0; i < 5 ; ++i) {
+		printf("%s from thread %d of %d\n", hello_world[i], omp_get_thread_num(), omp_get_num_threads());
+
+	}
 
   return 0;
 }
