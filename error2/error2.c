10b087df9c4f4f0fd748c053d9d2fcb8b0bf3390
Julian Merkle <juli.merkle@gmail.com>
2018-02-17 13:59:01 +0100

    A02b

--- error2/error2.c
+++ error2/error2.c
@@ -45,7 +45,10 @@ omp_init_lock(&lockb);
       omp_set_lock(&locka);
       for (i=0; i<N; i++)
         a[i] = i * DELTA;
-      omp_set_lock(&lockb);
+			omp_unset_lock(&locka);
+
+      omp_set_lock(&locka);
+			omp_set_lock(&lockb);
       printf("Thread %d adding a[] to b[]\n",tid);
       for (i=0; i<N; i++)
         b[i] += a[i];
@@ -59,12 +62,15 @@ omp_init_lock(&lockb);
       omp_set_lock(&lockb);
       for (i=0; i<N; i++)
         b[i] = i * PI;
+			omp_unset_lock(&lockb);
+
       omp_set_lock(&locka);
+			omp_set_lock(&lockb);
       printf("Thread %d adding b[] to a[]\n",tid);
       for (i=0; i<N; i++)
         a[i] += b[i];
-      omp_unset_lock(&locka);
       omp_unset_lock(&lockb);
+      omp_unset_lock(&locka);
       }
     }  /* end of sections */
   }  /* end of parallel region */
