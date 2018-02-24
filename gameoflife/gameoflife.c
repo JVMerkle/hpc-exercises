86d5da0e5ea9d5f897e93bd9effe0a3e6cd011fa
Micha Obertsein <Micha.Oberstein@outlook.com>
2018-02-24 14:51:18 +0100

    [Not correct] Implement Thread output

--- gameoflife/gameoflife.c
+++ gameoflife/gameoflife.c
@@ -15,9 +15,8 @@
 
 long TimeSteps = 100;
 
-void writeVTK2(long timestep, double *data, char prefix[1024], long w, long h) {
-  char filename[2048];  
-  int x,y; 
+void writeVTK2(char filename[2048], double *data, char prefix[1024], long w, long h) {
+  int x,y;
   
   long offsetX=0;
   long offsetY=0;
@@ -25,12 +24,11 @@ void writeVTK2(long timestep, double *data, char prefix[1024], long w, long h) {
   float deltay=1.0;
   long  nxy = w * h * sizeof(float);  
 
-  snprintf(filename, sizeof(filename), "%s-%05ld%s", prefix, timestep, ".vti");
   FILE* fp = fopen(filename, "w");
 
   fprintf(fp, "<?xml version=\"1.0\"?>\n");
   fprintf(fp, "<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\" header_type=\"UInt64\">\n");
-  fprintf(fp, "<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", offsetX, offsetX + w, offsetY, offsetY + h, 0, 0, deltax, deltax, 0.0);
+  fprintf(fp, "<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", offsetX, offsetX + w, offsetY, offsetY + h, 0, 0, deltax, deltay, 0.0);
   fprintf(fp, "<CellData Scalars=\"%s\">\n", prefix);
   fprintf(fp, "<DataArray type=\"Float32\" Name=\"%s\" format=\"appended\" offset=\"0\"/>\n", prefix);
   fprintf(fp, "</CellData>\n");
@@ -75,13 +73,9 @@ int count_living_neighbours(double *currentfield, int x, int y, int w, int h) {
     return number;
 } 
 
-void evolve(double* currentfield, double* newfield, int sqr_block_number, int w, int h) {
-	int thread_num = omp_get_thread_num();
-	int offset_y = (thread_num / sqr_block_number) * SLICE_SIZE;
-	int offset_x = (thread_num % sqr_block_number) * SLICE_SIZE;
-
-	for (int y = offset_y; y < offset_y + SLICE_SIZE; y++) {
-		for (int x = offset_x; x < offset_x + SLICE_SIZE; x++) {
+void evolve(double* currentfield, double* newfield, int w, int h) {
+	for (int y = 0; y < SLICE_SIZE; y++) {
+		for (int x = 0; x < SLICE_SIZE; x++) {
 
 			int number = count_living_neighbours(currentfield, x, y, w, h);
 			if (currentfield[calcIndex(w, x, y)] > 0) {
@@ -109,6 +103,10 @@ void game(int sqr_block_number, int w, int h) {
 	omp_set_num_threads(sqr_block_number * sqr_block_number);
 	#pragma omp parallel
 	{
+        int thread_num = omp_get_thread_num();
+        int offset_y = (thread_num / sqr_block_number) * SLICE_SIZE;
+        int offset_x = (thread_num % sqr_block_number) * SLICE_SIZE;
+
 		long t;
 		for (t=0;t<TimeSteps;t++) {
 			#pragma omp single
@@ -116,16 +114,19 @@ void game(int sqr_block_number, int w, int h) {
 				show(currentfield, w, h);
 			}
 
-			evolve(currentfield, newfield, sqr_block_number, w, h);
+            int index = calcIndex(w, offset_x, offset_y);
+
+            evolve(&currentfield[index], newfield, w, h);
 			#pragma omp barrier
 
-			#pragma omp single
-			{
-				printf("%ld timestep\n", t);
-				writeVTK2(t, currentfield, "gol", w, h);
-			}
+            printf("%ld timestep\n", t);
+            char filename[2048];
+            snprintf(filename, sizeof(filename), "t%d-%05ld%s", thread_num, t, ".vti");
 
-			usleep(200000);
+
+            writeVTK2(filename, currentfield, "gol", w, h);
+
+			getchar();
 
 
 			#pragma omp single
