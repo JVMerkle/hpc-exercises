861ae9ca0eb34166fc69eb1e94ae9b1e24e57a41
Micha Oberstein <Micha.Oberstein@outlook.com>
2018-03-13 16:05:54 +0100

    Get the stupid vtk output correct

--- gameoflife-mpi/main.c
+++ gameoflife-mpi/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 #include "mpi.h"
+
+#define calcIndex(width, x, y)  ((y)*(width) + (x))
+
+void writeVTK(char *filename, const char *field, int block_width, int block_height, int total_width, int total_height,
+           int offset_x, int offset_y);
+
+void *init_field(int rank, char *current_field, int width, int height) {
+    srand(rank*time(NULL));
+    for (int i = width; i < width * (height - 2); i++) {
+        current_field[i] = (char) ((rand() < RAND_MAX / 10) ? 1 : 0);
+    }
+}
 
 int main(int argc, char *argv[]) {
     int comm_world_rank, comm_world_size;
@@ -41,16 +54,59 @@ int main(int argc, char *argv[]) {
 
     // -----  -----
     int total_length = height * width;
-    int partition = total_length / comm_gol_size;
-    int offset_start = partition * comm_gol_rank;
-    int offset_end = (offset_start + partition < total_length)? offset_start + partition -1 : total_length -1;
+    int proc_height = height / comm_gol_size;
+    int proc_area = proc_height * width;
+    int offset = proc_area * comm_gol_rank;
 
-    printf("[INIT] Process %d of %d started with size of %dx%d - assigned partition %d-%d next rank: %d previous rank: %d\n",
-           comm_gol_rank, comm_gol_size, height, width, offset_start, offset_end,
+    printf("[INIT] Process %d of %d started with size of %dx%d - assigned partition %d next rank: %d previous rank: %d\n",
+           comm_gol_rank, comm_gol_size, height, width, offset,
            next_neighbour, previous_neighbour);
 
+    // Initialise fields
+    char *currentField = calloc((size_t) proc_area + 2 * width, sizeof(char));
+    char *nextField = calloc((size_t) proc_area + 2 * width, sizeof(char));
 
+    init_field(comm_gol_rank, currentField, width, proc_height + 2);
+
+    char thread_filename[2048];
+    snprintf(thread_filename, sizeof(thread_filename), "gol-%05d-r%d%s", 0, comm_gol_rank, ".vti");
+    writeVTK(thread_filename, currentField, width, proc_height, width, height, 0, comm_gol_rank * proc_height);
 
     MPI_Finalize();
     return 0;
+}
+
+
+void writeVTK(char *filename, const char *field, int block_width, int block_height, int total_width, int total_height,
+           int offset_x, int offset_y) {
+    int x, y;
+    float deltax = 1.0;
+    float deltay = 1.0;
+    long nxy = total_width * total_height * sizeof(float);
+
+    FILE *fp = fopen(filename, "w");
+
+    fprintf(fp, "<?xml version=\"1.0\"?>\n");
+    fprintf(fp, "<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\" header_type=\"UInt64\">\n");
+    fprintf(fp, "<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", offset_x,
+            offset_x + block_width, total_height - offset_y - block_height, total_height - offset_y, 0, 0, deltax,
+            deltay, 0.0);
+    fprintf(fp, "<CellData Scalars=\"%s\">\n", "gol");
+    fprintf(fp, "<DataArray type=\"Float32\" Name=\"%s\" format=\"appended\" offset=\"0\"/>\n", "gol");
+    fprintf(fp, "</CellData>\n");
+    fprintf(fp, "</ImageData>\n");
+    fprintf(fp, "<AppendedData encoding=\"raw\">\n");
+    fprintf(fp, "_");
+    fwrite((unsigned char *) &nxy, sizeof(long), 1, fp);
+
+    for (y = block_height; y >= 1; y--) {
+        for (x = 0; x < block_width; x++) {
+            float value = field[calcIndex(total_width, x, y)];
+            fwrite((unsigned char *) &value, sizeof(float), 1, fp);
+        }
+    }
+
+    fprintf(fp, "\n</AppendedData>\n");
+    fprintf(fp, "</VTKFile>\n");
+    fclose(fp);
 }
